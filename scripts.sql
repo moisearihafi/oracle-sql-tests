e754a9299ad18246315ffa9c764619d58ad2fb4b
moisearihafi <rwariindamoisee@gmail.com>
2024-10-03 00:27:57 +0300

    Add files via upload
    Scripts Query used to create our tables

---
+++ scripts.sql
@@ -0,0 +1,30 @@
+--create teachers table
+CREATE TABLE teachers (
+    teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+    teacher_name VARCHAR(50) NOT NULL,
+    
+);
+
+--create subjects table
+CREATE TABLE subjects (
+    subject_id INT AUTO_INCREMENT PRIMARY KEY,
+    subject_name VARCHAR(50) NOT NULL,
+    teacher_id INT,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(teacher_id) ON DELETE SET NULL
+);
+
+-- Create students table
+CREATE TABLE students (
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+
+);
+
+-- Create enrollments table (many-to-many relationship)
+CREATE TABLE enrollments (
+    student_id INT,
+    subject_id INT,
+    enroll_date DATE,
+    FOREIGN KEY (student_id) REFERENCES students(student_id) ON DELETE CASCADE,
+    FOREIGN KEY (subject_id) REFERENCES subjects(subject_id) ON DELETE CASCADE
+);
